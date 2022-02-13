52fc8bbf5d46af4f529ad2fb2024fd5bee8943d6
SashaShark14 <93825914+SashaShark14@users.noreply.github.com>
2022-02-13 23:44:08 +0300

    task finished

--- src/index.js
+++ src/index.js
@@ -38,7 +38,51 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const strLength = expr.length / 10
+const arrTen = new Array(strLength)
+for (let i = 0, o = 0; i < strLength; ++i, o += 10) {
+    arrTen[i] = expr.substr(o, 10)
+  }
+
+ const newArrTwo = arrTen.map(item => {
+  const itemLength = item.length / 2
+  const arrTwo = new Array(itemLength)
+  for (let i = 0, o = 0; i < itemLength; ++i, o += 2) {
+  arrTwo[i] = item.substr(o, 2)   
+  }
+ return arrTwo
+})
+const alfArr = newArrTwo.map((item)=> {
+ const newItem = item.map(el => {
+    if(el=== '00') {
+          return ''
+      }else if (el === '10') {
+        return '.'
+    } else if(el=== '11') {
+        return '-'
+    } else {
+        return ' '
+    }
+    return el     
+ })
+ return newItem      
+  });
+ 
+ const glArr = alfArr.map(item => item.join(''))
+ const morseArr = glArr.map(item => {
+    let newMorse = item === undefined?  ' ' : MORSE_TABLE[item]
+    return newMorse
+ })
+
+ const arr1 = morseArr.map(item => {
+    if (item === undefined) {
+    return ' '
+  } else {
+    return item
+  }
+ })
+ return arr1.join('')
+ 
 }
 
 module.exports = {
